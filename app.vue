75a5f79801b2ba143e23bef258947c70280a4a81
hamed <amzhamed@gmail.com>
2024-11-11 16:37:13 -0800

    Add background

--- app.vue
+++ app.vue
@@ -57,10 +57,10 @@ onUnmounted(() => {
 </script>
 
 <template>
-<!--  <div class="w-full h-[60vh] opacity-90 absolute px-32 -top-[40vh]">-->
-<!--    <div class="w-full h-full blur-[300px] bg-gradient-to-br from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%"/>-->
-<!--    <div class="overflow-y-auto w-1 z-0 scrollbar-hidden size-0">.</div>-->
-<!--  </div>-->
+  <div class="w-full h-[60vh] blur-3xl opacity-80 absolute px-32 -top-[40vh]">
+    <div class="w-full h-full rounded-full blur-[100px] bg-gradient-to-r from-pink-500 from-10% via-sky-500 via-30% to-indigo-500 to-90%"/>
+    <div class="overflow-y-auto w-1 z-0 scrollbar-hidden size-0">.</div>
+  </div>
 
   <main ref="main" class="max-w-5xl mx-auto px-4 overflow-visible">
 
